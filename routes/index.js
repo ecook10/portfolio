bf7a2e70e155e9521af6a39811ce5c0622085d1a
ecook10 <evan.cook10@icloud.com>
2015-10-09 14:33:12 -0700

    created portfolio database

--- routes/index.js
+++ routes/index.js
@@ -59,13 +59,11 @@ router.get('/', function(req, res) {
 
                 //get latest blog entry
                 Entry.findOne(function(err, blog_data) {
-                    console.log('BLOG_DATA:');
-                    console.log(blog_data);
 
                     res.render('index', {
                         title: 'home',
                         activeid: 'home',
-                        latest: blog_data,
+                        latest: {},
                         tweets: twit_data
                     });
                 });
